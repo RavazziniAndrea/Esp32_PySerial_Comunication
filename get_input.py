e67ca685048b24df122f99f2e9171c65f93a4661
andrea <andrea.ravazzini@horsa.it>
2023-03-25 21:51:05 +0100

    Added port comment

--- get_input.py
+++ get_input.py
@@ -1,5 +1,7 @@
 import serial
 
+#In Linux, the esp32 is usually identified by /dev/ttyUSB0.
+#In Windows, it uses a COM port
 s = serial.Serial("/dev/ttyUSB0",115200)
 while True:
     print(s.readline())
