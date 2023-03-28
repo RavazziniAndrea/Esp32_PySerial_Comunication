766fc0ced6e4c53294890c681d6ed1661d488a94
andrea <ravazziniandrea@hotmail.it>
2023-03-28 22:32:54 +0200

    Specified linux command

--- get_input.py
+++ get_input.py
@@ -1,6 +1,6 @@
 import serial
 
-#In Linux, the esp32 is usually identified by /dev/ttyUSB0.
+#In Linux, the esp32 is usually identified by /dev/ttyUSB0 ($ ls /dev/tty*).
 #In Windows, it uses a COM port
 s = serial.Serial("/dev/ttyUSB0",115200)
 while True:
